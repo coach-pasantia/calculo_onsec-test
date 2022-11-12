cbab40c1b478591961e00f0f0392893b9e3cbe07
k07 <107227754+coach-pasantia@users.noreply.github.com>
2022-11-12 04:45:34 +0000

    update api

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ const {dbConnection} = require('./db/dbConnection.js');
 class Server{
     constructor(){
         this.app = express();
-        this.port = process.env.PORT || 3000;
+        this.port = process.env.PORT || 8080;
         this.apiUser=`/api/v1/user`;
         this.apiProduct =`/api/v1/product`;
         this.apiEmpleados = `/api/v1/empleados`;
@@ -25,7 +25,7 @@ class Server{
         this.app.use(morgan("dev"))
         this.app.use(express.urlencoded({extended:false}))
         this.app.use(express.json())
-        this.app.use(express.static('./backend/public'))
+        this.app.use(express.static('./api/public'))
     }
     routes(){
         this.app.use(this.apiUser,router.user);
